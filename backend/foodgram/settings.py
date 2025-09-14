629a83dbde15e462a9801ae3e1043a9c482d1bec
Ольга Пырьянова <pyryanova@mail.ru>
2025-09-14 10:17:56 +0300

    «1.0»

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -135,7 +135,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = "ru-ru"
-TIME_ZONE = os.getenv("TIME_ZONE", "Europe/Berlin")
+TIME_ZONE = os.getenv("TIME_ZONE", "Europe/Moscow")
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
