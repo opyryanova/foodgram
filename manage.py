c3b6b75a0876bed597a54b4a4d41cc42b2b735e3
Ольга Пырьянова <pyryanova@mail.ru>
2025-09-19 13:40:37 +0300

    «3.0»

--- manage.py
+++ manage.py
@@ -4,10 +4,7 @@ import sys
 
 def main():
     """Запуск административных команд Django."""
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE",
-        "foodgram.settings"
-    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodgram.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
