c6bda26975c16258605a65feeedd6daac2470870
Ольга Пырьянова <pyryanova@mail.ru>
2025-09-21 10:52:05 +0300

    «4.0»

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -212,17 +212,37 @@ SESSION_COOKIE_SAMESITE = os.getenv("SESSION_COOKIE_SAMESITE", "Lax")
 CSRF_COOKIE_SAMESITE = os.getenv("CSRF_COOKIE_SAMESITE", "Lax")
 
 LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "simple": {"format": "[{levelname}] {message}", "style": "{"}
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[{asctime}] [{levelname}] {name}:{funcName}:{lineno} {message}',
+            'style': '{',
+        },
     },
-    "handlers": {
-        "console": {"class": "logging.StreamHandler", "formatter": "simple"}
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
     },
-    "root": {
-        "handlers": ["console"],
-        "level": "DEBUG",
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'rest_framework': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'api': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
     },
 }
 
